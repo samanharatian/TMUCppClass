f5be396c706f2134f261a7848214b8b7b401f693
Farhad Jems <farhadjems@gmail.com>
2022-03-25 17:52:40 +0430

    chapter 01 implemented

---
+++ 01-Course Program, Learning a Programming Language, Installing Editor and Compiler/01-test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hello world\n";
+    // note that if you want help form vs code for 
+    // auto completing and ...
+    // you must determine the type of file when you are naming it 
+    // and dont forget to install extension! they are so helpful :D
+}
